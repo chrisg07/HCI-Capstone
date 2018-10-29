e4977b31a66493ecdbd2a44d1a3efa80ceafeafc
Christopher Guilliams <chris.guilliams@modea.com>
2018-10-29 19:53:02 -0400

    Created header, body, and footer components

--- CapstoneProject/src/app/app.module.ts
+++ CapstoneProject/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
