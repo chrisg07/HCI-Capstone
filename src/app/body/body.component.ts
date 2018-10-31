550c8d8fef569b1e1c322a0ae9bfc738b4180f38
Christopher Guilliams <chris.guilliams@modea.com>
2018-10-30 20:42:02 -0400

    installed topojson

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import * as topojson from 'topojson';
 
 @Component({
   selector: 'app-body',
@@ -6,16 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
+  svg;
+  myProjection;
+  path;
+  graticule;
+  virginia;
 
   constructor() { }
 
   ngOnInit() {
-    d3.json('../../assets/jsoncounties-VA.json', function(error, uk) {
-      if (error) {
-        return console.error(error);
-      }
-      console.log(uk);
+    const svg = d3.select('svg');
+    const myProjection = d3.geoNaturalEarth1();
+    const path = d3.geoPath().projection(myProjection);
+    const graticule = d3.geoGraticule();
+    this.virginia = d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json').then(() => {
+      console.log(this);
     });
+    this.drawMap(this.virginia);
   }
 
+  drawMap(state) {
+    const svg = d3.select('svg');
+
+    svg.append('path')
+      .datum(this.graticule)
+      .attr('class', 'graticule')
+      .attr('d', this.path);
+
+    svg.append('path')
+      .datum(this.graticule.outline)
+      .attr('class', 'foreground')
+      .attr('d', this.path);
+
+    svg.append('g')
+      .selectAll('path')
+      .data(this.virginia.feature(state, state.objects.counties).features)
+      .enter().append('path')
+      .attr('d', this.path);
+  }
 }
