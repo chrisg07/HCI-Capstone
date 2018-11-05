82be83493318b1168603a5934cd2c317aabf2a51
Christopher Guilliams <chris.guilliams@modea.com>
2018-11-05 10:19:53 -0500

    Drew a map!

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -8,42 +8,78 @@ import * as topojson from 'topojson';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
-  svg;
-  myProjection;
-  path;
-  graticule;
-  virginia;
+  private svg;
+  private myProjection;
+  private path;
+  private virginia;
+  private width = 960;
+  private height = 500;
 
   constructor() { }
 
   ngOnInit() {
-    const svg = d3.select('svg');
-    const myProjection = d3.geoNaturalEarth1();
-    const path = d3.geoPath().projection(myProjection);
-    const graticule = d3.geoGraticule();
-    this.virginia = d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json').then(() => {
-      console.log(this);
+    this.svg = d3.select('app-body')
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height);
+
+    this.myProjection = d3.geoConicConformal()
+      .parallels([38 + 2 / 60, 39 + 12 / 60])
+      .rotate([78 + 30 / 60, 0])
+      .scale(8000)
+      .translate([0, 0]);
+
+    this.path = d3.geoPath().projection(this.myProjection);
+    // load TopoJSON data
+    d3.json('../../assets/va-counties.json').then((value) => {
+      this.virginia = value;
+      this.drawMap(this.virginia);
     });
-    this.drawMap(this.virginia);
   }
 
+  /**
+   * Draw a map from TopoJSON data
+   * @param state the TopoJSON to draw
+   */
   drawMap(state) {
-    const svg = d3.select('svg');
+    const stateOutline = topojson.feature(state, state.objects.states);
+    const counties = this.path.bounds(stateOutline);
 
-    svg.append('path')
-      .datum(this.graticule)
-      .attr('class', 'graticule')
-      .attr('d', this.path);
+    this.myProjection
+      .translate([this.width / 2 - (counties[0][0] + counties[1][0]) / 2, this.height / 2 - (counties[0][1] + counties[1][1]) / 2]);
 
-    svg.append('path')
-      .datum(this.graticule.outline)
-      .attr('class', 'foreground')
-      .attr('d', this.path);
+    this.svg.append('path')
+      .datum(stateOutline)
+      .attr('class', 'state')
+      .attr('d', this.path)
+      .attr('fill', '#ccc');
 
-    svg.append('g')
+    this.svg.append('path')
+      .datum(topojson.mesh(state, state.objects.counties, function(a, b) { return a !== b; }))
+      .attr('class', 'county-border')
+      .attr('d', this.path)
+      .attr('fill', 'none')
+      .attr('stroke', '#fff')
+      .attr('stroke-width', '1.01px')
+      .attr('stroke-linejoin', 'round')
+      .attr('stroke-linecap', 'round');
+    /*
+    this.svg.append('g')
       .selectAll('path')
-      .data(this.virginia.feature(state, state.objects.counties).features)
-      .enter().append('path')
-      .attr('d', this.path);
+      .data(state.features.counties)
+      .enter()
+      .append('path')
+      .attr('d', this.path)
+      .style('stroke', '#fff')
+      .style('stroke-width', '1');
+
+     this.svg.selectAll('path')
+      .data(this.virginia.features)
+      .enter()
+      .append('path')
+      .attr('d', this.path)
+      .style('stroke', '#fff')
+      .style('stroke-width', '1');
+      */
   }
 }
