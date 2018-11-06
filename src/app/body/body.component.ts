6d9aab0b53ed05089ec3182e24f9edb7da41a360
Christopher Guilliams <chris.guilliams@modea.com>
2018-11-06 16:33:06 -0500

    drew a county

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -10,18 +10,31 @@ import * as topojson from 'topojson';
 export class BodyComponent implements OnInit {
   private svg;
   private myProjection;
+  private myCountyProjection;
   private path;
+  private countyPath;
   private virginia;
+  private counties;
+  private countiesList;
   private width = 960;
   private height = 500;
+  private county1SVG;
+  private county1Width = 400;
+  private county1Height = 200;
 
   constructor() { }
 
   ngOnInit() {
+    this.countiesList = [];
     this.svg = d3.select('app-body')
       .append('svg')
       .attr('width', this.width)
       .attr('height', this.height);
+
+    this.county1SVG = d3.select('app-body')
+      .append('svg')
+      .attr('width', this.county1Width)
+      .attr('height', this.county1Height);
 
     this.myProjection = d3.geoConicConformal()
       .parallels([38 + 2 / 60, 39 + 12 / 60])
@@ -30,9 +43,12 @@ export class BodyComponent implements OnInit {
       .translate([0, 0]);
 
     this.path = d3.geoPath().projection(this.myProjection);
+    
     // load TopoJSON data
     d3.json('../../assets/va-counties.json').then((value) => {
+      this.counties = value;
       this.virginia = value;
+      this.buildCountiesList(this.virginia);
       this.drawMap(this.virginia);
     });
   }
@@ -41,10 +57,10 @@ export class BodyComponent implements OnInit {
    * Draw a map from TopoJSON data
    * @param state the TopoJSON to draw
    */
-  drawMap(state) {
+  private drawMap(state) {
     const stateOutline = topojson.feature(state, state.objects.states);
     const counties = this.path.bounds(stateOutline);
-
+    console.log(this.counties);
     this.myProjection
       .translate([this.width / 2 - (counties[0][0] + counties[1][0]) / 2, this.height / 2 - (counties[0][1] + counties[1][1]) / 2]);
 
@@ -55,7 +71,8 @@ export class BodyComponent implements OnInit {
       .attr('fill', '#ccc');
 
     this.svg.append('path')
-      .datum(topojson.mesh(state, state.objects.counties, function(a, b) { return a !== b; }))
+      .datum(topojson.mesh(state, state.objects.counties, function(a, b) {
+        return a !== b; }))
       .attr('class', 'county-border')
       .attr('d', this.path)
       .attr('fill', 'none')
@@ -63,23 +80,47 @@ export class BodyComponent implements OnInit {
       .attr('stroke-width', '1.01px')
       .attr('stroke-linejoin', 'round')
       .attr('stroke-linecap', 'round');
-    /*
-    this.svg.append('g')
-      .selectAll('path')
-      .data(state.features.counties)
-      .enter()
-      .append('path')
-      .attr('d', this.path)
-      .style('stroke', '#fff')
-      .style('stroke-width', '1');
 
-     this.svg.selectAll('path')
-      .data(this.virginia.features)
-      .enter()
-      .append('path')
-      .attr('d', this.path)
-      .style('stroke', '#fff')
-      .style('stroke-width', '1');
-      */
+      console.log(this.countiesList);
+      this.drawCounty('Frederick County');
+  }
+
+  private buildCountiesList(state) {
+    for (const county of state.objects.counties.geometries) {
+      const newCounty = {[county.properties.name] : county};
+      this.countiesList[county.properties.name] = county;
+    }
+    console.log(this.countiesList);
+  }
+
+  private drawCounty(name: string) {
+    const curCounty = this.countiesList[name];
+    const geoCounty = this.counties.objects.counties.geometries.filter(function(d) {
+      if (d.properties.name === name) {
+        return d;
+      }
+    });
+    this.counties.objects.counties.geometries = geoCounty;
+    console.log(this.virginia);
+    console.log(this.counties);
+    const county = topojson.feature(this.counties, this.counties.objects.counties);
+    
+    this.myCountyProjection = d3.geoIdentity()
+      .fitSize([this.county1Width, this.county1Height], county);
+      this.countyPath = d3.geoPath().projection(this.myCountyProjection);
+      const bounds = this.countyPath.bounds(county);
+    // console.log(geoCounty);
+
+    console.log(county);
+    console.log(bounds);
+    //his.myCountyProjection
+    //    .translate([this.county1Width / 2 - (bounds[0][0] + bounds[1][0]) / 2, this.county1Height / 2 - (bounds[0][1] + bounds[1][1]) / 2]);
+
+    this.county1SVG.append('path')
+        .datum(county)
+        .attr('class', 'county')
+        .attr('d', this.countyPath)
+        .attr('fill', '#ccc')
+        .attr('stroke', '#ccc');
   }
 }
