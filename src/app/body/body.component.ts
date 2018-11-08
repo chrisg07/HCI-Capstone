b2d7984b6584e5f4d6e602728fcf86846193cac1
Christopher Guilliams <chris.guilliams@modea.com>
2018-11-08 12:09:17 -0500

    Updated second county dropdown

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -13,24 +13,27 @@ import { HostListener } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  private svg;
-  private myProjection;
-  private myCountyProjection;
-  private path;
+  private stateSVG;
+  private stateProjection;
+  private countyProjection;
+  private statePath;
   private countyPath;
   private virginia;
   private counties;
-  public countiesList;
+  public countiesList = [];
   public stateWidth = 960;
   public stateHeight = 500;
-  private county1SVG;
+  private firstCountySVG;
+  private firstCounty;
   public firstCountyWidth;
   public firstCountyHeight;
-  public myControl = new FormControl();
-  private autocompleteOptions: string[] = new Array<string>();
-  public filteredOptions: Observable<string[]>;
+  public firstCountyControl = new FormControl();
+  private firstCountyOptions: string[] = new Array<string>();
+  public firstCountyFilteredOptions: Observable<string[]>;
+  public secondCountyControl = new FormControl();
+  private secondCountyOptions: string[] = new Array<string>();
+  public secondCountyFilteredOptions: Observable<string[]>;
   private resizeTimeout;
-  private geoCounty;
 
   @HostListener('window:resize')
     onWindowResize() {
@@ -39,58 +42,34 @@ export class BodyComponent implements OnInit {
             clearTimeout(this.resizeTimeout);
         }
         this.resizeTimeout = setTimeout((() => {
-            console.log(window.innerWidth);
-            console.log(this.firstCountyHeight);
-            console.log(this.firstCountyWidth);
-            this.stateWidth = window.innerWidth * .75;
-            this.stateHeight = this.stateWidth * .521;
-            /*
-            const stateOutline = topojson.feature(this.virginia, this.virginia.objects.states);
-            const counties = this.path.bounds(stateOutline);
-            this.myProjection
-              .translate([this.stateWidth / 2 -
-                (counties[0][0] + counties[1][0]) / 2, this.stateHeight / 2 - (counties[0][1] + counties[1][1]) / 2]);
-            d3.select('.Virginia')
-              .attr('width', this.stateWidth)
-              .attr('height', this.stateHeight);
-            d3.select('.VirginiaMap')
-              .attr('width', this.stateWidth)
-              .attr('height', this.stateHeight);
-            */
-           this.drawMap(this.virginia);
+          this.updateStateMap();
+          this.updateFirstCountyMap();
+          this.updateSecondCountyMap();
         }).bind(this), 500);
     }
   constructor() { }
 
   ngOnInit() {
-    this.countiesList = [];
-
-    this.county1SVG = d3.select('.firstCountyContainer')
+    this.firstCountySVG = d3.select('.firstCountyContainer')
       .append('svg')
       .attr('class', 'firstCounty')
       .attr('width', this.firstCountyWidth)
       .attr('height', this.firstCountyHeight);
 
-    this.path = d3.geoPath().projection(this.myProjection);
+    this.statePath = d3.geoPath().projection(this.stateProjection);
     // load TopoJSON data
     d3.json('../../assets/va-counties.json').then((value) => {
       this.virginia = value;
       this.counties = this.virginia;
-      this.buildCountiesList(this.virginia);
       this.drawMap(this.virginia);
-      this.autocompleteOptions = Object.keys(this.countiesList);
-      this.filteredOptions = this.myControl.valueChanges
-      .pipe(
-        startWith(''),
-        map(filterValue => this._filter(filterValue))
-      );
+      this.populateDropdowns();
     });
   }
 
   private getMapData() {
     d3.json('../../assets/va-counties.json').then((value) => {
       this.virginia = value;
-      this.counties = this.virginia;
+      this.counties = value;
     });
   }
   /**
@@ -99,34 +78,40 @@ export class BodyComponent implements OnInit {
    */
   private drawMap(state) {
     this.clearStateMap();
-    this.svg = d3.select('.Virginia')
+    this.stateSVG = d3.select('.Virginia')
       .append('svg')
       .attr('class', 'VirginiaMap')
       .attr('width', this.stateWidth)
       .attr('height', this.stateHeight);
 
     const stateOutline = topojson.feature(state, state.objects.states);
-    const counties = this.path.bounds(stateOutline);
-    this.myProjection = d3.geoIdentity()
+    const counties = this.statePath.bounds(stateOutline);
+    this.stateProjection = d3.geoIdentity()
         .reflectY(true)
         .fitSize([this.stateWidth, this.stateHeight], stateOutline);
-      this.path = d3.geoPath().projection(this.myProjection);
-    this.svg.append('path')
+      this.statePath = d3.geoPath().projection(this.stateProjection);
+    this.stateSVG.append('path')
       .datum(stateOutline)
       .attr('class', 'state')
-      .attr('d', this.path)
+      .attr('d', this.statePath)
       .attr('fill', '#ccc');
 
-    this.svg.append('path')
+    this.stateSVG.append('path')
       .datum(topojson.mesh(state, state.objects.counties, function(a, b) {
         return a !== b; }))
       .attr('class', 'county-border')
-      .attr('d', this.path)
+      .attr('d', this.statePath)
       .attr('fill', 'none')
       .attr('stroke', '#fff')
       .attr('stroke-width', '1.01px')
       .attr('stroke-linejoin', 'round')
       .attr('stroke-linecap', 'round');
+  }
+
+  private updateStateMap() {
+    this.stateWidth = window.innerWidth * .75;
+    this.stateHeight = this.stateWidth * .521;
+    this.drawMap(this.virginia);
   }
 
   private buildCountiesList(state) {
@@ -135,33 +120,33 @@ export class BodyComponent implements OnInit {
     }
   }
 
-  private drawCounty(name: string) {
+  private findCounty(name: string): Object {
     for (const county of this.virginia.objects.counties.geometries) {
       if (county.properties.name === name) {
-        console.log('county found in virginia geometries');
-        console.log(county);
-        this.geoCounty = county;
-        break;
+        return county;
       }
     }
-    if (this.geoCounty) {
+    return null;
+  }
+
+  private drawFirstCounty(name: string) {
+    this.firstCounty = this.findCounty(name);
+    if (this.firstCounty) {
       this.clearFirstCounty();
-      this.counties.objects.counties = this.geoCounty;
-      console.log(this.counties);
-      console.log(this.counties.objects.counties);
+      this.counties.objects.counties = this.firstCounty;
       const county = topojson.feature(this.counties, this.counties.objects.counties);
-      this.myCountyProjection = d3.geoIdentity()
+      this.countyProjection = d3.geoIdentity()
         .reflectY(true)
         .fitSize([this.firstCountyWidth, this.firstCountyHeight], county);
 
-      this.countyPath = d3.geoPath().projection(this.myCountyProjection);
-      this.county1SVG = d3.select('.firstCountyContainer')
+      this.countyPath = d3.geoPath().projection(this.countyProjection);
+      this.firstCountySVG = d3.select('.firstCountyContainer')
         .append('svg')
         .attr('class', 'firstCounty')
         .attr('width', this.firstCountyWidth)
         .attr('height', this.firstCountyHeight);
 
-      this.county1SVG.append('path')
+      this.firstCountySVG.append('path')
         .datum(county)
         .attr('class', 'county')
         .attr('d', this.countyPath)
@@ -173,19 +158,58 @@ export class BodyComponent implements OnInit {
     this.getMapData();
   }
 
+  private updateFirstCountyMap() {
+
+  }
+
+  private drawSecondCounty(name: string) {
+
+  }
+
+  private updateSecondCountyMap() {
+
+  }
+
   private clearFirstCounty() {
-    this.county1SVG.remove();
+    this.firstCountySVG.remove();
   }
 
   private clearStateMap() {
     d3.select('.VirginiaMap').remove();
   }
 
-  private _filter(value: string): string[] {
+  private populateDropdowns() {
+    this.buildCountiesList(this.virginia);
+    this.firstCountyOptions = Object.keys(this.countiesList);
+    this.firstCountyFilteredOptions = this.firstCountyControl.valueChanges
+    .pipe(
+      startWith(''),
+      map(filterValue => this._firstFilter(filterValue))
+    );
+    this.secondCountyOptions = Object.keys(this.countiesList);
+    this.secondCountyFilteredOptions = this.secondCountyControl.valueChanges
+    .pipe(
+      startWith(''),
+      map(filterValue => this._secondFilter(filterValue))
+    );
+  }
+
+  private _firstFilter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    const results = this.autocompleteOptions.filter(option => option.toLowerCase().includes(filterValue));
+    console.log(filterValue);
+    const results = this.firstCountyOptions.filter(option => option.toLowerCase().includes(filterValue));
     if (results.length === 1 && results[0].toLowerCase() === filterValue) {
-      this.drawCounty(results[0]);
+      this.drawFirstCounty(results[0]);
+    }
+    return results;
+  }
+
+  private _secondFilter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    console.log(filterValue);
+    const results = this.secondCountyOptions.filter(option => option.toLowerCase().includes(filterValue));
+    if (results.length === 1 && results[0].toLowerCase() === filterValue) {
+      this.drawSecondCounty(results[0]);
     }
     return results;
   }
